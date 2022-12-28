b26dab2f586627741b34e45c917f04dab041f350
drozennn <rafialyahya@gmail.com>
2022-12-29 00:33:28 +0700

    Membua desain form Hapus Penyewa

--- PenyewaanMobil/HapusPenyewa.Designer.vb
+++ PenyewaanMobil/HapusPenyewa.Designer.vb
@@ -26,56 +26,89 @@ Partial Class HapusPenyewa
         Me.NamaPenyewaHapus = New System.Windows.Forms.Label()
         Me.BtnBatal = New System.Windows.Forms.Button()
         Me.BtnKonfirmasi = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(15, 28)
+        Me.Label1.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(73, 81)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(285, 15)
+        Me.Label1.Size = New System.Drawing.Size(422, 20)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Apakah anda yakin ingin menghapus data penyewa :"
         '
         'NamaPenyewaHapus
         '
         Me.NamaPenyewaHapus.AutoSize = True
-        Me.NamaPenyewaHapus.Location = New System.Drawing.Point(133, 77)
+        Me.NamaPenyewaHapus.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.NamaPenyewaHapus.ForeColor = System.Drawing.Color.Red
+        Me.NamaPenyewaHapus.Location = New System.Drawing.Point(242, 129)
         Me.NamaPenyewaHapus.Name = "NamaPenyewaHapus"
-        Me.NamaPenyewaHapus.Size = New System.Drawing.Size(41, 15)
+        Me.NamaPenyewaHapus.Size = New System.Drawing.Size(68, 20)
         Me.NamaPenyewaHapus.TabIndex = 1
         Me.NamaPenyewaHapus.Text = "Label2"
         Me.NamaPenyewaHapus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'BtnBatal
         '
-        Me.BtnBatal.Location = New System.Drawing.Point(48, 138)
+        Me.BtnBatal.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnBatal.Location = New System.Drawing.Point(121, 185)
         Me.BtnBatal.Name = "BtnBatal"
-        Me.BtnBatal.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBatal.Size = New System.Drawing.Size(90, 29)
         Me.BtnBatal.TabIndex = 2
         Me.BtnBatal.Text = "Tidak"
         Me.BtnBatal.UseVisualStyleBackColor = True
         '
         'BtnKonfirmasi
         '
-        Me.BtnKonfirmasi.Location = New System.Drawing.Point(180, 138)
+        Me.BtnKonfirmasi.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnKonfirmasi.Location = New System.Drawing.Point(347, 185)
         Me.BtnKonfirmasi.Name = "BtnKonfirmasi"
-        Me.BtnKonfirmasi.Size = New System.Drawing.Size(75, 23)
+        Me.BtnKonfirmasi.Size = New System.Drawing.Size(88, 29)
         Me.BtnKonfirmasi.TabIndex = 3
         Me.BtnKonfirmasi.Text = "Ya"
         Me.BtnKonfirmasi.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Red
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(-4, -2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(567, 66)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Franklin Gothic Demi", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(155, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(262, 41)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Hapus Penyewa"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'HapusPenyewa
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(312, 190)
+        Me.ClientSize = New System.Drawing.Size(560, 252)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.BtnKonfirmasi)
         Me.Controls.Add(Me.BtnBatal)
         Me.Controls.Add(Me.NamaPenyewaHapus)
         Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Name = "HapusPenyewa"
         Me.Text = "Hapus Penyewa"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -85,4 +118,6 @@ Partial Class HapusPenyewa
     Friend WithEvents NamaPenyewaHapus As Label
     Friend WithEvents BtnBatal As Button
     Friend WithEvents BtnKonfirmasi As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
 End Class
--- PenyewaanMobil/Penyewa.Designer.vb
+++ PenyewaanMobil/Penyewa.Designer.vb
@@ -26,57 +26,102 @@ Partial Class Penyewa
         Me.BtnTambah = New System.Windows.Forms.Button()
         Me.BtnEdit = New System.Windows.Forms.Button()
         Me.BtnRemove = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.DataGridPenyewa, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridPenyewa
         '
         Me.DataGridPenyewa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridPenyewa.Location = New System.Drawing.Point(127, 37)
+        Me.DataGridPenyewa.Location = New System.Drawing.Point(306, 110)
         Me.DataGridPenyewa.Name = "DataGridPenyewa"
         Me.DataGridPenyewa.RowTemplate.Height = 25
-        Me.DataGridPenyewa.Size = New System.Drawing.Size(445, 252)
+        Me.DataGridPenyewa.Size = New System.Drawing.Size(453, 252)
         Me.DataGridPenyewa.TabIndex = 0
         '
         'BtnTambah
         '
-        Me.BtnTambah.Location = New System.Drawing.Point(24, 47)
+        Me.BtnTambah.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnTambah.Location = New System.Drawing.Point(35, 33)
         Me.BtnTambah.Name = "BtnTambah"
-        Me.BtnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.BtnTambah.Size = New System.Drawing.Size(123, 35)
         Me.BtnTambah.TabIndex = 1
         Me.BtnTambah.Text = "Tambah"
         Me.BtnTambah.UseVisualStyleBackColor = True
         '
         'BtnEdit
         '
-        Me.BtnEdit.Location = New System.Drawing.Point(24, 87)
+        Me.BtnEdit.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnEdit.Location = New System.Drawing.Point(164, 33)
         Me.BtnEdit.Name = "BtnEdit"
-        Me.BtnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEdit.Size = New System.Drawing.Size(130, 35)
         Me.BtnEdit.TabIndex = 2
         Me.BtnEdit.Text = "Edit"
         Me.BtnEdit.UseVisualStyleBackColor = True
         '
         'BtnRemove
         '
-        Me.BtnRemove.Location = New System.Drawing.Point(24, 126)
+        Me.BtnRemove.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnRemove.Location = New System.Drawing.Point(300, 33)
         Me.BtnRemove.Name = "BtnRemove"
-        Me.BtnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRemove.Size = New System.Drawing.Size(133, 35)
         Me.BtnRemove.TabIndex = 3
         Me.BtnRemove.Text = "Hapus"
         Me.BtnRemove.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Moccasin
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.BtnRemove)
+        Me.GroupBox1.Controls.Add(Me.BtnEdit)
+        Me.GroupBox1.Controls.Add(Me.BtnTambah)
+        Me.GroupBox1.Location = New System.Drawing.Point(-3, -5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(787, 87)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Franklin Gothic Demi", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(493, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(261, 41)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Daftar Penyewa"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.PenyewaanMobil.My.Resources.Resources.pluto_riding_a_car
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 110)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(265, 252)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
         '
         'Penyewa
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(632, 322)
-        Me.Controls.Add(Me.BtnRemove)
-        Me.Controls.Add(Me.BtnEdit)
-        Me.Controls.Add(Me.BtnTambah)
+        Me.ClientSize = New System.Drawing.Size(786, 387)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.DataGridPenyewa)
         Me.Name = "Penyewa"
         Me.Text = "Penyewa"
         CType(Me.DataGridPenyewa, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -85,4 +130,7 @@ Partial Class Penyewa
     Friend WithEvents BtnTambah As Button
     Friend WithEvents BtnEdit As Button
     Friend WithEvents BtnRemove As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
